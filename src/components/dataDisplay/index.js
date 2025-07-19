caa837b6deb6d76465825a39ed603ce8819ff609
Giovanni Amanam Udoaka <gio2.udoaka@gmail.com>
2025-07-19 12:07:47 +0100

    feat(dataDisplay): Add Carousel component and showcase

--- src/components/dataDisplay/index.js
+++ src/components/dataDisplay/index.js
@@ -1,4 +1,5 @@
 // src/components/dataDisplay/index.js
 
 export { default as Card } from './Card.vue'
-// Add other dataDisplay components here as they are developed (e.g., Carousel, DataTable)
+export { default as Carousel } from './Carousel.vue' // NEW: Export Carousel
+// Add other dataDisplay components here as they are developed (e.g., DataTable)
--- src/router/components/dataDisplay.component.routes.js
+++ src/router/components/dataDisplay.component.routes.js
@@ -2,6 +2,7 @@
 
 // Import component showcase views for the dataDisplay category
 import CardShowcase from '@/views/ui-components/dataDisplay/CardShowcase.vue'
+import CarouselShowcase from '@/views/ui-components/dataDisplay/CarouselShowcase.vue' // NEW: Import CarouselShowcase
 
 const dataDisplayComponentRoutes = [
   {
@@ -9,6 +10,12 @@ const dataDisplayComponentRoutes = [
     name: 'ComponentShowcaseCards',
     component: CardShowcase,
     meta: { requiresAuth: true, title: 'Cards Showcase' },
+  },
+  {
+    path: 'dataDisplay/carousel', // NEW: Path for Carousel showcase
+    name: 'ComponentShowcaseCarousel', // NEW: Name for Carousel showcase
+    component: CarouselShowcase, // NEW: CarouselShowcase component
+    meta: { requiresAuth: true, title: 'Carousel Showcase' },
   },
   // Add other dataDisplay component routes here as they are developed
 ]
